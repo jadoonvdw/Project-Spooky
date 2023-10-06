c204775e3bfc577a780ac6bd8231b4b5411df855
chjlling <138952642+chjlling@users.noreply.github.com>
2023-10-05 22:17:16 -0700

    Dramatic Spawn Improvements
    Spawn Improvements
    -Added separate car and spawn collision to keep players in
    -Slightly reduced fog density
    -Slightly increased car headlight visibility in fog
    -Randomized headlight flicker
    -Added headlight flicker sfx
    -Added car radio sfx
    -Added boulders behind car and in crater in front of spawn implying land slide
    -Added new trees to the right of spawn after exiting to fill up blank space
    
    Project Improvements
    -Reorganized nodes in main

--- World/car.gd
+++ World/car.gd
@@ -1,13 +1,26 @@
 extends Node3D
 @onready var animplayer = $AnimationPlayer
-
-
+@onready var rng = RandomNumberGenerator.new()
+@onready var timerTime = 0.3
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
 	animplayer.play("flicker")
-	pass # Replace with function body.
-
-
+	$lightFlickerTimer.start(timerTime)
+	$lightFlickerTimer.timeout.connect(_on_timer_timeout)
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _on_timer_timeout(): 
+	#print("Timer Stop") #For testing the timer
+	$lightBuzz.play()
+	animplayer.play("flicker")
+	rng.randomize
+	timerTime = rng.randf_range(0.3, 1.5) #Change these values to change rate of car lights flickering
+	$lightFlickerTimer.start(timerTime)
+	
+
+	
